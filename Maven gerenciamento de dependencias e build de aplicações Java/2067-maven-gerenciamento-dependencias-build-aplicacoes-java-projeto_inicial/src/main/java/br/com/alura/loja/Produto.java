3587740ae357be357cddfcb1c4a5290b0efdbe58
pabullo teste <pabulloddt@gmail.com>
2024-09-04 17:40:24 -0300

    Adicionado Trilha de curso Alura Maven gerenciamento de dependências e build de aplicações Java

---
+++ Maven gerenciamento de dependencias e build de aplicações Java/2067-maven-gerenciamento-dependencias-build-aplicacoes-java-projeto_inicial/src/main/java/br/com/alura/loja/Produto.java
@@ -0,0 +1,21 @@
+package br.com.alura.loja;
+
+import java.math.BigDecimal;
+
+public class Produto {
+
+	public Produto(String string, BigDecimal ten) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Object getNome() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Object getPreco() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
